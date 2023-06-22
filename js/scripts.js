a3f41d9a8f19ebdf8aaed5851cec74a53039b944
Brian Scherner <brianscherner@gmail.com>
2023-06-22 11:35:05 -0700

    Create Address constructor. Update Contact constructor. Update displayContactDetails function to display multiple email addresses. Create createEmailAddresses function. Update handleFromSubmission function to include new selectors for new email addresses.

--- js/scripts.js
+++ js/scripts.js
@@ -28,12 +28,17 @@ AddressBook.prototype.deleteContact = function(id) {
 	return true;
 };
 
-function Contact(firstName, lastName, phoneNumber, emailAddress, physicalAddress) {
+function Address(addressType, addressDetail) {
+	this.type = addressType;
+	this.detail = addressDetail;
+}
+
+function Contact(firstName, lastName, phoneNumber, emailAddresses, physicalAddresses) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.phoneNumber = phoneNumber;
-	this.emailAddress = emailAddress;
-	this.physicalAddress = physicalAddress;
+	this.emailAddresses = emailAddresses;
+	this.physicalAddresses = physicalAddresses;
 }
 
 Contact.prototype.fullName = function() {
@@ -61,8 +66,16 @@ function displayContactDetails(event) {
 	document.querySelector(".first-name").innerText = contact.firstName;
 	document.querySelector(".last-name").innerText = contact.lastName;
 	document.querySelector(".phone-number").innerText = contact.phoneNumber;
-	document.querySelector(".email-address").innerText = contact.emailAddress;
-	document.querySelector(".physical-address").innerText = contact.physicalAddress;
+	let emailAddressDiv = document.querySelector("div#email-address-list");
+	emailAddressDiv.innerHTML = "";
+	const ul = document.createElement("ul");
+	contact.emailAddresses.forEach(function(emailAddress) {
+		const li = document.createElement("li");
+		li.append("(" + emailAddress.type + ") " + emailAddress.detail);
+		ul.append(li);
+	});
+	emailAddressDiv.append(ul);
+	document.querySelector(".physical-address").innerText = contact.physicalAddresses.detail;
 	document.querySelector("button.delete").setAttribute("id", contact.id);
 	document.querySelector("div#contact-details").removeAttribute("class");
 }
@@ -74,20 +87,39 @@ function handleDelete(event) {
 	listContacts(addressBook);
 }
 
+function createEmailAddresses() {
+	let arrayOfAddresses = [];
+	for(index = 1; index <= 3; index++) {
+		let emailValueTargetString = "input#new-email-address" + index;
+		let emailTypeTargetString = "input[name='email-address-type" + index + "']:checked";
+		let emailAddress = document.querySelector(emailValueTargetString).value;
+		let emailType = document.querySelector(emailTypeTargetString).value;
+		if (emailAddress.length !== 0) {
+			let newAddress = new Address(emailType, emailAddress);
+			arrayOfAddresses.push(newAddress);
+		}
+	}
+	return arrayOfAddresses;
+}
+
 function handleFormSubmission(event) {
 	event.preventDefault();
+	createEmailAddresses();
 	const inputtedFirstName = document.querySelector("input#new-first-name").value;
 	const inputtedLastName = document.querySelector("input#new-last-name").value;
 	const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-	const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
 	const inputtedPhysicalAddress = document.querySelector("input#new-physical-address").value;
-	let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedPhysicalAddress);
+	const addressTwo = new Address("physical", inputtedPhysicalAddress);
+	const emailAddresses = createEmailAddresses();
+	let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, emailAddresses, addressTwo);
 	addressBook.addContact(newContact);
 	listContacts(addressBook);
 	document.querySelector("input#new-first-name").value = null;
 	document.querySelector("input#new-last-name").value = null;
 	document.querySelector("input#new-phone-number").value = null;
-	document.querySelector("input#new-email-address").value = null;
+	document.querySelector("input#new-email-address1").value = null;
+	document.querySelector("input#new-email-address2").value = null;
+	document.querySelector("input#new-email-address3").value = null;
 	document.querySelector("input#new-physical-address").value = null;
 }
 
