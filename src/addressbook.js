b45041ccd54e6e1576dbb268a892db86260c2dbf
Brian Scherner <brianscherner@gmail.com>
2023-07-25 10:41:57 -0700

    Fix spacing.

--- src/addressbook.js
+++ src/addressbook.js
@@ -1,30 +1,30 @@
 export default function AddressBook() {
-	this.contacts = {};
-	this.currentId = 0;
+  this.contacts = {};
+  this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
-	contact.id = this.assignId();
-	this.contacts[contact.id] = contact;
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
 };
 
 AddressBook.prototype.assignId = function() {
-	this.currentId += 1;
-	return this.currentId;
+  this.currentId += 1;
+  return this.currentId;
 };
 
 AddressBook.prototype.findContact = function(id) {
-	if (this.contacts[id] !== undefined) {
-		return this.contacts[id];
-	}
-	return false;
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
 };
 
 AddressBook.prototype.deleteContact = function(id) {
-	if (this.contacts[id] === undefined) {
-		return false;
-	}
-	delete this.contacts[id];
-	return true;
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
 };
 
