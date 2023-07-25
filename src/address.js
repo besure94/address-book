be806d3cbdee687c3909a46bd38e2cb43a1c450d
Brian Scherner <brianscherner@gmail.com>
2023-07-25 10:41:37 -0700

    Fix spacing.

--- src/address.js
+++ src/address.js
@@ -1,4 +1,4 @@
 export default function Address(addressType, addressDetail) {
-	this.type = addressType;
-	this.detail = addressDetail;
+  this.type = addressType;
+  this.detail = addressDetail;
 }
